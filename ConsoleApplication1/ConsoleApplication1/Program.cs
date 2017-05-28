67b56831480064144ab4ae6163299f2bc1dc8469
Timo <cobravspanda01@gmail.com>
2017-05-28 14:22:25 +0200

    werkt, maar stackoverflow
    dus werkt niet hihihihihi

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,10 @@ namespace ConsoleApplication1
     {
         static int N = 9;
         //Array to store sudoku puzzle in
-        static int[,] sudoku = new int[N, N];
-        
+        static int[,] sudoku;
+        static bool[,] unchangable;
+        static bool backwards = false;
+        const string direction = "left-right";
         static void Main(string[] args)
         {
             while (true)
@@ -23,10 +25,13 @@ namespace ConsoleApplication1
                 N = firstLine.Length;
                 //make the array to store the sudoku
                 sudoku = new int[N, N];
+                unchangable = new bool[N, N];
                 //store the first line
                 for(int k =0; k < N; k++)
                 {
-                    sudoku[0, k] = int.Parse(firstLine[k]);
+                    int number = int.Parse(firstLine[k]);
+                    if (number != 0) unchangable[0, k] = true;
+                    sudoku[0, k] = number;
                 }
                 //store remaining lines
                 for (int i = 1; i < N; i++)
@@ -36,11 +41,74 @@ namespace ConsoleApplication1
                     //Store numbers in sudoku array
                     for (int j = 0; j < N; j++)
                     {
+                        int number = int.Parse(numbers[j]);
+                        if (number != 0) unchangable[i, j] = true;
                         sudoku[i, j] = int.Parse(numbers[j]);
                     }
                 }
-                //print the current sudoku puzzle
-                print_sudoku();
+                BackTrack(0, 0);
+            }
+        }
+        static void MoveNext(int row, int col)
+        {
+            backwards = false;
+            //Solution found
+            if (row == N - 1 && col == N - 1) {
+                Console.WriteLine("foundsolution");
+                return;
+            }
+            switch (direction)
+            {
+                case "left-right":
+                    {
+                        //Backtrack next variable
+                        if (col < N - 1) { BackTrack(row, col + 1); }
+                        else { BackTrack(row + 1, 0); }
+                    }
+                    break;
+            }
+        }
+        static void MoveBack(int row, int col)
+        {
+            backwards = true;
+            //Reset the current variable
+            sudoku[row, col] = 0;
+            switch (direction)
+            {
+                case "left-right":
+                    {
+                        //Backtrack previous variable
+                        if (col ==0) { BackTrack(row-1, N-1); }
+                        else { BackTrack(row, col-1); }
+                    }
+                    break;
+            }
+        }
+        static void BackTrack(int row, int col)
+        {
+            print_sudoku();
+            //If the variable was given, move to the next or previous
+            if (unchangable[row, col]) {
+                if (backwards) MoveBack(row, col);
+                else MoveNext(row, col);
+            }
+            else
+            {
+                //If the number can still be incremented
+                if (sudoku[row, col] < N){
+                    //increment it by 1
+                    sudoku[row, col]++;
+
+                    //If this doesnt create a violation
+                    if (!Violation(row, col)){
+                        //Move on to the next variable
+                        MoveNext(row, col);
+                    }
+                    //Otherwise backtrack the same variable
+                    else BackTrack(row, col);
+                }
+                //If the number cant be incremented, moveback to the previous variable
+                else { MoveBack(row, col); }
             }
         }
         static void print_sudoku()
@@ -56,33 +124,37 @@ namespace ConsoleApplication1
             }
             Console.WriteLine("----------------------------");
         }
-        static bool Violation( int number, int row, int col )
+        static bool Violation(int row, int col )
         {
-            if ( RowViolation( number, row, col )|| ColViolation( number, row, col )|| BoxViolation( number, row, col ) ) return true;
+            if ( RowViolation(row, col )|| ColViolation(row, col )|| BoxViolation(row, col ) ) return true;
 
             return false;
         }
 
-        static bool RowViolation( int number, int row, int col )
+        static bool RowViolation(int row, int col )
         {
+            int number = sudoku[row, col];
             // Check for violations in a row
             for (int i = 0; i < N; i++)
-                if (sudoku[row, i] == number) return true;
-
+            {
+                if (sudoku[row, i] == number&&i!=col) return true;
+            }
             return false;
         }
 
-        static bool ColViolation( int number, int row, int col )
+        static bool ColViolation(int row, int col )
         {
+            int number = sudoku[row, col];
             // Check for violations in a column
             for (int i = 0; i < N; i++)
-                if (sudoku[i, col] == number) return true;
+                if (sudoku[i, col] == number&&i!=row) return true;
 
             return false;
         }
 
-        static bool BoxViolation( int number, int row, int col )
+        static bool BoxViolation(int row, int col )
         {
+            int number = sudoku[row, col];
             int beginRow = 0;
             int beginCol = 0;
             int sN = (int)Math.Sqrt(N);
@@ -114,7 +186,7 @@ namespace ConsoleApplication1
             // Check for box violation
             for (int i = 0; i < sN; i++)
                 for (int j = 0; j < sN; j++)
-                    if (sudoku[beginRow + i, beginCol + j] == number) return true;
+                    if (sudoku[beginRow + i, beginCol + j] == number&&!(beginRow+i==row&&beginCol+j==col)) return true;
 
             return false;
         }
